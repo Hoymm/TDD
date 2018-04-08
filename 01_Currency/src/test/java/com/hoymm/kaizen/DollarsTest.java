a4d790eb595ed506686f886e6f16d8f3be6cc5d2
Hoymm <Damian.Andrzej.Muca@gmail.com>
2018-04-08 22:39:53 +0200

    Change times method inside Dollar class to actually create and return new Dollar object instead of changing value of the object that it's been invoked on

--- 01_Currency/src/test/java/com/hoymm/kaizen/DollarsTest.java
+++ 01_Currency/src/test/java/com/hoymm/kaizen/DollarsTest.java
@@ -7,8 +7,10 @@ public class DollarsTest {
 
     @Test
     public void testMultiplication(){
-        Dollar money = new Dollar(30);
-        money.times(2);
-        assertEquals(60, money.amount);
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
     }
 }
