3eaf2ab71eeb04e72cac653b192953234025a08d
Hoymm <Damian.Andrzej.Muca@gmail.com>
2018-04-09 21:13:53 +0200

    Remove testFrancMultiplication() because now multiplication on Dollar currency and Franc has the same logic (they're in the same class) and therefore there is no need to test that logic twice

--- src/main/java/com/hoymm/kaizen/Franc.java
+++
@@ -1,7 +0,0 @@
-package com.hoymm.kaizen;
-
-class Franc extends Money {
-    Franc(int amount, String currency) {
-        super(amount, currency);
-    }
-}
--- src/test/java/com/hoymm/kaizen/MoneyTest.java
+++ src/test/java/com/hoymm/kaizen/MoneyTest.java
@@ -8,17 +8,10 @@ import static org.junit.Assert.assertFalse;
 public class MoneyTest {
 
     @Test
-    public void testDollarMultiplication(){
+    public void testMultiplication(){
         Money five = Money.dollar(5);
         assertEquals(Money.dollar(10), five.times(2));
         assertEquals(Money.dollar(15), five.times(3));
-    }
-
-    @Test
-    public void testFrancMultiplication(){
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
     }
 
     @Test
