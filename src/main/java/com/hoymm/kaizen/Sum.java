08d8159cbd3968861e6eb649f5438114cebba4f9
Damian <Damian.Andrzej.Muca@gmail.com>
2018-04-10 15:31:36 +0200

    Fix times() method inside Sum.class

--- src/main/java/com/hoymm/kaizen/Sum.java
+++ src/main/java/com/hoymm/kaizen/Sum.java
@@ -22,6 +22,6 @@ class Sum implements Expression{
 
     @Override
     public Expression times(int multiplier){
-        return new Sum(augend.times(multiplier).addend.times(multiplier));
+        return new Sum(augend.times(multiplier), addend.times(multiplier));
     }
 }
