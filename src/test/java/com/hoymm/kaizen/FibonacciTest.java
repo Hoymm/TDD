0e964307c49cbf62e891b370e4c3f1fb077401b2
Hoymm <Damian.Andrzej.Muca@gmail.com>
2018-04-19 22:40:31 +0200

    Remove commented code, change static import

--- src/test/java/com/hoymm/kaizen/FibonacciTest.java
+++ src/test/java/com/hoymm/kaizen/FibonacciTest.java
@@ -4,6 +4,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FibonacciTest {
     private Fibonacci fibonacci;
 
@@ -14,37 +16,37 @@ public class FibonacciTest {
 
     @Test
     public void fibonacciIndex0_shouldReturnValue0(){
-        Assert.assertEquals(0, fibonacci.getValueOf(0));
+        assertEquals(0, fibonacci.getValueOf(0));
     }
 
     @Test
     public void fibonacciIndex1_shouldReturnValue1(){
-        Assert.assertEquals(1, fibonacci.getValueOf(1));
+        assertEquals(1, fibonacci.getValueOf(1));
     }
 
     @Test
     public void fibonacciIndex2_shouldReturnValue1(){
-        Assert.assertEquals(1, fibonacci.getValueOf(2));
+        assertEquals(1, fibonacci.getValueOf(2));
     }
 
     @Test
     public void fibonacciIndex3_shouldReturnValue2(){
-        Assert.assertEquals(2, fibonacci.getValueOf(3));
+        assertEquals(2, fibonacci.getValueOf(3));
     }
 
     @Test
     public void fibonacciIndex4_shouldReturnValue3(){
-        Assert.assertEquals(3, fibonacci.getValueOf(4));
+        assertEquals(3, fibonacci.getValueOf(4));
     }
 
     @Test
     public void fibonacciIndex5_shouldReturnValue5(){
-        Assert.assertEquals(5, fibonacci.getValueOf(5));
+        assertEquals(5, fibonacci.getValueOf(5));
     }
 
     @Test (timeout = 20)
     public void searchForIndex60_shouldGive1548008755920InUnder20ms(){
-        Assert.assertEquals(1548008755920L, fibonacci.getValueOf(60));
+        assertEquals(1548008755920L, fibonacci.getValueOf(60));
     }
 
     @Test (expected = IllegalArgumentException.class)
--- src/test/java/com/hoymm/kaizen/MoneyTest.java
+++ src/test/java/com/hoymm/kaizen/MoneyTest.java
@@ -28,12 +28,6 @@ public class MoneyTest {
     }
 
     @Test
-    public void testSum(){
-        //Money sum = Money.dollar(5).plus(Money.dollar(5));
-        //assertEquals(Money.dollar(10), sum);
-    }
-
-    @Test
     public void testSimpleAddition(){
         Money five = Money.dollar(5);
         Expression sum = five.plus(five);
